57949dbb307bd845de6986c13de79177a4d7df35
Çağatay <durucagatay01@gmail.com>
2024-02-19 14:48:05 +0300

    Simple Ball Logic

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,82 @@
 use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
+
+const BALL_SPEED: f32 = 5.;
+const BALL_SIZE: f32 = 5.;
+
+#[derive(Component)]
+struct Velocity(Vec2);
+
+#[derive(Component)]
+struct Position(Vec2);
+
+#[derive(Component)]
+struct Ball;
+
+#[derive(Bundle)]
+struct BallBundle {
+    ball: Ball,
+    velocity: Velocity,
+    position: Position
+}
+
+
+impl BallBundle {
+    fn new(x: f32, y: f32) -> Self {
+        Self {
+            ball: Ball,
+            velocity: Velocity(Vec2::new(x, y)),
+            position: Position(Vec2::new(0., 0.)),
+        }
+    }
+}
 
 fn main() {
     App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (spawn_ball, spawn_camera))
+        .add_systems(Update, (project_positions))
         .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn_empty()
+            .insert(Camera2dBundle::default());
+}
+
+fn spawn_ball(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    println!("Spawning ball...");
+
+    let mesh = Mesh::from(shape::Circle::new(BALL_SIZE));
+    let material = ColorMaterial::from(Color::rgb(1., 0., 0.));
+
+    let mesh_handle = meshes.add(mesh);
+    let material_handle = materials.add(material);
+
+    commands.spawn((
+        BallBundle::new(1., 0.),
+        MaterialMesh2dBundle {
+            mesh: mesh_handle.into(),
+            material: material_handle,
+            ..default()
+        },
+    ));
+}
+
+fn move_ball(mut ball: Query<(&mut Position, &Velocity), With<Ball>>) {
+    if let Ok((mut position, velocity)) = ball.get_single_mut() {
+        position.0 += velocity.0
+    }
+}
+
+fn project_positions(
+    mut positionables: Query<(&mut Transform, &Position)>
+) {
+    for (mut transform, position) in &mut positionables {
+        transform.translation = position.0.extend(0.);
+    }
 }
